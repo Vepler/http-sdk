85fe1735b58b0070448b7636dee66aaea3d36d15
Goran Nikic <goran@nikic.co>
2025-04-17 16:20:36 +0100

    fix: update schools host URL to propbar domain

--- src/config.ts
+++ src/config.ts
@@ -25,7 +25,7 @@ const defaultConfig = {
     areaReferenceHost: 'https://api2.propbar.co.uk/area-reference',
     crimeHost: 'https://api2.propbar.co.uk/crime',
     roverHost: 'https://api2.propbar.co.uk/rover',
-    schoolsHost: 'https://api2.vepler.co.uk/schools',
+    schoolsHost: 'https://api2.propbar.co.uk/schools',
     timeout: 60000, // 60 seconds default timeout
     logLevel: 'info',
     headers: {
@@ -37,7 +37,7 @@ const defaultConfig = {
     areaReferenceHost: 'https://api2.propbar.co.uk/area-reference',
     crimeHost: 'https://api2.propbar.co.uk/crime',
     roverHost: 'https://api2.propbar.co.uk/rover',
-    schoolsHost: 'https://api2.vepler.co.uk/schools',
+    schoolsHost: 'https://api2.propbar.co.uk/schools',
     timeout: 60000, // 60 seconds default timeout
     logLevel: 'debug',
     headers: {
