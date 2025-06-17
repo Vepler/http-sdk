1e9e8d653ac945a204bd5e242d1a285b203079a7
Goran Nikic <goran@nikic.co>
2025-06-17 06:20:03 +0100

    feat: remove street autocomplete functionality
    Remove the deprecated street autocomplete endpoint and related code

--- src/services/location/service.ts
+++ src/services/location/service.ts
@@ -1,10 +1,8 @@
 import { autocompleteLocation } from './routes/autocomplete-location';
-import { autocompleteStreet } from './routes/autocomplete-street';
 
 // Re-export types if needed
 // export { ... };
 
 export default {
   autocomplete: autocompleteLocation,
-  autocompleteStreet: autocompleteStreet,
 };
